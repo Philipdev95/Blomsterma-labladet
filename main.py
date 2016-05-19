c98cc3f1e2b2f1f1cce1ee76e7444038a9d80208
emmaajohaansson <emmakristina95@hotmail.com>
2016-05-19 12:26:06 +0200

    instruktioner

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # *-* coding: utf-8 *-*
+import MySQLdb
+# connect
+db = MySQLdb.connect(host="localhost", user="root", passwd="abcd1234", db="af9121", charset="utf8")
+# create a cursor
+cursor = db.cursor()
+# execute SQL statement
+cursor.execute("select Namn, Medlemsnr from Medlem")
+# get the resultset as a tuple
+result = cursor.fetchall()
+# iterate through resultset
+for record in result:
+	print record[0] , "har medlemsnummer", record[1]
