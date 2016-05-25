dfdc9e7b0b8871541aff31be265c747c16b5cbb0
Philipdev95 <philipdev@icloud.com>
2016-05-25 11:19:49 +0200

    tabs vs spaces

--- main.py
+++ main.py
@@ -12,25 +12,16 @@ def print_puff():
     cursor.execute("select Rubrik, Ingress from Artiklar limit 10")
     result = cursor.fetchall()
     for record in result:
-	print record[0], record[3]
+        print record[0], record[3]
     return template("start")
 
 @route('<main:re:.*\.css>',name='static')
 def css(main):
     print "css:",main
     return static_file(main,root='./static',mimetype='text/css')
-    
-<<<<<<< HEAD
-
-=======
-@route("/")
-def start():
-    return template("start")
->>>>>>> origin/TPL
 
 @route("/article/")
 def article():
     return template("article")
-
 
 run(host='localhost', port=8080)
