b719d3648fd7e7644170c29b8f80b1ce9934f8fc
emmaajohaansson <emmakristina95@hotmail.com>
2016-05-25 11:43:45 +0200

    öjoujoö

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import MySQLdb
 import bottle
 from bottle import route, run, template, Bottle, request, redirect
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="abcd1234", db="projekt", charset="utf8")
+db = MySQLdb.connect(host="localhost", user="root", passwd="hejhej123", db="projekt", charset="utf8")
 cursor = db.cursor()
 
     
@@ -12,7 +12,7 @@ def print_puff():
     cursor.execute("select Rubrik, Ingress from Artiklar limit 10")
     result = cursor.fetchall()
     for record in result:
-	print record[0], record[3]
+        print record[0], record[1]
     return template("start")
 
 @route('<main:re:.*\.css>',name='static')
@@ -20,17 +20,5 @@ def css(main):
     print "css:",main
     return static_file(main,root='./static',mimetype='text/css')
     
-<<<<<<< HEAD
-
-=======
-@route("/")
-def start():
-    return template("start")
->>>>>>> origin/TPL
-
-@route("/article/")
-def article():
-    return template("article")
-
-
 run(host='localhost', port=8080)
+
